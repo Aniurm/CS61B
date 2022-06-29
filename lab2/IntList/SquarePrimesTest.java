a68ea4a4dc83d280a83dfef419b9ff013c760257
ANIURM <534147085@qq.com>
2022-06-29 16:21:42 +0800

    Finish Lab2, the first time. It seems good

--- lab2/IntList/SquarePrimesTest.java
+++ lab2/IntList/SquarePrimesTest.java
@@ -17,4 +17,23 @@ public class SquarePrimesTest {
         assertEquals("14 -> 15 -> 16 -> 289 -> 18", lst.toString());
         assertTrue(changed);
     }
+
+    //all inputs are composite numbers
+    @Test
+    public void testSquarePrimesAllComposite() {
+       IntList lst = IntList.of(20, 4, 6, 8, 10);
+       boolean changed = IntListExercises.squarePrimes(lst);
+       assertEquals("20 -> 4 -> 6 -> 8 -> 10", lst.toString());
+       assertTrue(!changed);
+    }
+
+    //all inputs are prime number
+    @Test
+    public void testSquarePrimesAllPrime() {
+        IntList lst = IntList.of(3, 5, 7, 11);
+        boolean changed = IntListExercises.squarePrimes(lst);
+        assertEquals("9 -> 25 -> 49 -> 121", lst.toString());
+        assertTrue(changed);
+    }
+
 }
